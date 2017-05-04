895c15d8b70ff6f914d4bf2a9c27094ff74ac71d
KirillSkrunnik <zaknifen@gmail.com>
2017-05-04 21:16:01 +0300

    second commit in phpshtor

--- hello.php
+++ hello.php
@@ -1,7 +1,28 @@
+<form method="POST">
+    <input type="number" name="num1" placeholder="Первое число"/>
+    <select name="sign">
+        <option>  + </option>
+        <option>  - </option>
+    </select>
+    <input type="number" name="num2" placeholder="Второе число"/>
+    <p><input type="submit"></p>
+    <p><?= isset($res) ? 'Результат' . $res : '' ?></p>
+    <p><?= isset($error) ? 'Ошибка' . $error : '' ?></p>
+</form>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 04.05.2017
- * Time: 20:47
- */
+if (isset($_POST['num1']) && isset($_POST['sign']) && isset($_POST['num2'])) {
+
+
+    switch ($_POST['sign']) {
+        case 'plus':
+            $res = $_POST['num1'] + (int)$_POST['num2'];
+            break;
+        case 'minus':
+            $res = $_POST['num1'] - (int)$_POST['num2'];
+            break;
+        default:
+            $error = 'Неправельный знак';
+    }
+}
+?>
